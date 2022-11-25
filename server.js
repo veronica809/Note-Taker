7a3f84cd99c9ddbd56d7182629f16bc2701875e4
Veronica Garcia <veronicagarcia@veronicas-mbp.attlocal.net>
2022-11-24 23:01:13 -0500

    db.json file created

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
 
 const PORT = 3001;
 const app = express();
 
+app.use(express.static("public"));
 
-app.get('/', (req, rees) =>{
-    res.send( `testing`)
+app.get("/", (req, res) => {
+  res.send("index.html");
+});
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("/api/notes", (req, res) => {
+  // Send a message to the client
+  res.sendFile(path.join(__dirname, "db.json"));
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get reviews`);
 });
 
-app.get('/api', (req, res) => {
-    res.json({
-      term: 'api',
-      description:
-        'An application programming interface, is a computing interface that defines interactions between multiple software intermediaries',
-    });
-  });
-  
-  app.listen(PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
-  );
-  
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);

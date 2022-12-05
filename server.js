04f8e0256199169667bdf0ea9a4329d71759eca5
Veronica Garcia <veronicagarcia@veronicas-mbp.attlocal.net>
2022-12-04 21:57:27 -0500

    heroku deployment issues

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const util = require("util");
 const uuid = require("./helpers/uuid");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
